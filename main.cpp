e161e698d695c0d69b6da09cc8c71e2029c5d849
glavoue <glavoue@liris.cnrs.fr>
2021-01-02 10:58:56 +0100

    Update main.cpp
    Changing License, from LGPLv3 to MPL2

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+// Copyright (c) 2020 University of Lyon and CNRS (France).
+// All rights reserved.
+//
+//The code is the implementation of our paper
+//"PCQM: A Full-Reference Quality Metric for Colored 3D Point Clouds"
+//Gabriel Meynet, Yana Nehme, Julie Digne, Guillaume Lavoué
+//Presented at QoMEX 2020.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+// WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
 /** @file */
 #include <algorithm>
 #include <chrono>
